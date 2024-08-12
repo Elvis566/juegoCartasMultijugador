ae2468aa746ec31a2512ed21aeb07e0d73ee36be
RobinsonZam025 <robinsonzambranos1@hotmail.com>
2024-08-11 23:05:07 -0500

    mejoraListaAmigos

--- Front-End/Interfaz/src/app/Pages/sala-espera/sala-espera.page.ts
+++ Front-End/Interfaz/src/app/Pages/sala-espera/sala-espera.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiNodeService } from 'src/app/Servicios/api-node.service';
 
 @Component({
   selector: 'app-sala-espera',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalaEsperaPage implements OnInit {
 
-  constructor() { }
+  amigos:any;
+  id: any = localStorage.getItem('idUser');
+
+  constructor(private aps: ApiNodeService) { }
 
   ngOnInit() {
+    this.getFriends();
+  }
+
+  getFriends(){
+    this.aps.getFriends(this.id).subscribe({
+      next:(data:any)=> {
+        this.amigos = data.friendsData
+        // this.avatar_id = data.friendsData.avatar
+      },
+      error:(e:any)=>{
+        console.log(e);
+      }
+    })
   }
 
 }
